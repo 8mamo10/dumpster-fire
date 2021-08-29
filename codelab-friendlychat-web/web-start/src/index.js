4e16f40c903c5a1ce489f940027bf775be5f662c
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-08-30 01:34:47 +0900

    Enable to display messages fetched from firestore

--- codelab-friendlychat-web/web-start/src/index.js
+++ codelab-friendlychat-web/web-start/src/index.js
@@ -89,7 +89,7 @@ function isUserSignedIn() {
 async function saveMessage(messageText) {
   // TODO 7: Push a new message to Cloud Firestore.
   try {
-    await addDoc(collection(getFirestore(), 'message'), {
+    await addDoc(collection(getFirestore(), 'messages'), {
       name: getUserName(),
       text: messageText,
       profilePicUrl: getProfilePicUrl(),
@@ -104,6 +104,18 @@ async function saveMessage(messageText) {
 // Loads chat messages history and listens for upcoming ones.
 function loadMessages() {
   // TODO 8: Load and listen for new messages.
+  const recentMessagesQuery = query(collection(getFirestore(), 'messages'), orderBy('timestamp', 'desc'), limit(12));
+
+  onSnapshot(recentMessagesQuery, function (snapshot) {
+    snapshot.docChanges().forEach(function (change) {
+      if (change.type === 'removed') {
+        deleteMessage(change.doc.id);
+      } else {
+        var message = change.doc.data();
+        displayMessage(change.doc.id, message.timestamp, message.name, message.text, message.profilePicUrl, message.imageUrl);
+      }
+    });
+  });
 }
 
 // Saves a new message containing an image in Firebase.
