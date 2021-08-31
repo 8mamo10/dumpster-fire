4407926543289789be36fdbb890b951945a824d5
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-09-01 03:24:02 +0900

    Enable to send notification

--- codelab-friendlychat-web/web-start/src/firebase-messaging-sw.js
+++ codelab-friendlychat-web/web-start/src/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 // TODO: Set up Firebase Cloud Messaging service worker
-import { initializeApp } from "@firebase/app";
-import { getMessaging } from "@firebase/messaging";
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging/sw";
 import { getFirebaseConfig } from "./firebase-config";
 
 const firebaseApp = initializeApp(getFirebaseConfig());
 getMessaging(firebaseApp);
-console.ingo('Firebase messaging service worker is set up');
+console.info('Firebase messaging service worker is set up');
