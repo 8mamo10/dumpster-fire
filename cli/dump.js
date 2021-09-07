256c6d51d0e867399041aaa73f0c6984a54ad801
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-09-07 23:59:14 +0900

    Handle arguments

--- cli/dump.js
+++ cli/dump.js
@@ -1,2 +1,13 @@
-console.log(process.argv[2])
-console.log(process.argv[3])
+//console.log(process.argv[2])
+//console.log(process.argv[3])
+
+const [, , firstArg] = process.argv;
+
+if (!firstArg) {
+  console.error("Please pass one argument!!");
+  process.exit(1);
+}
+
+const msg = `Hello ${firstArg}`;
+
+console.log(msg);
