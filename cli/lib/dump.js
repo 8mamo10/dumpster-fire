ed19a3d91091177af3a3eb437d2fa3691280adbd
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-09-09 23:52:16 +0900

    Do npm init

---
+++ cli/lib/dump.js
@@ -0,0 +1,27 @@
+const readline = require("readline");
+
+module.exports = () => {
+  const [, , firstArg] = process.argv;
+  if (!firstArg) {
+    console.error("Please pass one argument!!");
+    process.exit(1);
+  }
+
+  const msg = `Hello ${firstArg}.`;
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  console.log(msg);
+
+  rl.question("Please enter some phrase: ", answer => {
+    console.log(`Thank you ! Let's start ${answer}`);
+
+    rl.close();
+  });
+}
+
+
+
