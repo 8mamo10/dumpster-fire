b565f9c3e2fac23ef2783f7471187b29f32b402a
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-09-19 11:52:07 +0900

    Dump data from firestore

--- dumper/dumper.js
+++ dumper/dumper.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const { credential } = require('firebase-admin');
+const { credential, firestore } = require('firebase-admin');
 
 const credentialPath = './serviceAccountKey.json';
 let outputFilePath = './dump.csv';
@@ -21,5 +21,27 @@ admin.initializeApp({
 const db = admin.firestore();
 
 console.log('[INFO] Start listren to Firestore.');
+
+let query = db.collection('messages');
+query.orderBy('timestamp', 'desc').get().then(querySnapshot => {
+  querySnapshot.forEach(documentSnapshot => {
+    console.log(`[INFO] Found document at ${documentSnapshot.ref.path}`);
+    data = JSON.stringify(documentSnapshot.data(), null, 2);
+    console.log(data);
+  });
+});
+
+
+
+/*
+const unsub = db.collection('messages').doc('test').onSnapshot(snapshot => {
+  console.log('[INFO] Firestore are changed.');
+  console.log(snapshot.data());
+}, error => {
+  console.error(`[ERROR] Can't observe firestore document.`)
+  console.error(`[ERROR] ${error}`)
+  process.exit(1)
+});
+*/
 
 console.log('[EXIT]');
