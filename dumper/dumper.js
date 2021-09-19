ffbad1dae81cb1e863b2de1753e3bd1112ff84d6
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-09-19 11:01:17 +0900

    Connect firestore

--- dumper/dumper.js
+++ dumper/dumper.js
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const { credential } = require('firebase-admin');
 
+const credentialPath = './serviceAccountKey.json';
 let outputFilePath = './dump.csv';
 
 program.option('-o, --out [path]', 'A path to the output file.').parse(process.argv)
 
 if (program.out) outputFilePath = program.out;
 
-console.log(program.out);
+const admin = require('firebase-admin');
+const serviceAccount = require(credentialPath);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 
+const db = admin.firestore();
+
+console.log('[INFO] Start listren to Firestore.');
+
+console.log('[EXIT]');
